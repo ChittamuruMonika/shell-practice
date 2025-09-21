9242a5eb0c38fa59b7e8125b26a72ae9cdebd1ee
ChittamuruMonika <chittamurumonika2001@gmail.com>
2025-09-21 15:28:28 +0530

    speccial var

---
+++ special_var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID os the script: $$"
+sleep 10 &
+echo "PID of the last command in background is: $!"
+
