978cd4deb3cb4c619ccacdbc10e17187ee65635b
ChittamuruMonika <chittamurumonika2001@gmail.com>
2025-09-21 15:12:54 +0530

    time cal

--- time_cal.sh
+++ time_cal.sh
@@ -7,7 +7,7 @@
 start_time=$(date +%s)
 sleep 10
 end_time=$(date +%s)
-Total_time=$((end_time-start_time))
+Total_time=$(($end_time-$start_time))
 echo "start_time is:$start_time"
 echo "end_time is:$end_time"
 echo "Total time is:$Total_time seconds" 
