07d1cf3bd41ba8b40de38348418806ad05d3f2af
ChittamuruMonika <chittamurumonika2001@gmail.com>
2025-09-21 16:07:22 +0530

    data types

--- datatypes.sh
+++ datatypes.sh
@@ -9,5 +9,7 @@ SUM=$(($NUMBER1+$NUMBER2))
 
 echo "Sum of numbers is: ${SUM}"
 
-LEADERS=("MODI" "PUTIN" "TRUMP")
+LEADERS=("MODI" "TRUMP" "PUTIN")
 echo "All leaders are: ${LEADERS[@]}"
+echo "FIRST LEADER IS: ${LEADERS[0]}"
+echo "Tenth leader is: ${{LEADERS[10]}"
