71a9fc07870e9f6921ea3158d12ff72b572c23fe
ChittamuruMonika <chittamurumonika2001@gmail.com>
2025-09-21 16:01:44 +0530

    data types

--- datatypes.sh
+++ datatypes.sh
@@ -3,6 +3,11 @@
 NUMBER1=100
 NUMBER2=200
 
+#everything in shell is considered as string"
+
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "Sum of numbers is: ${SUM}"
+
+LEADERS=("MODI" "PUTIN" "TRUMP")
+echo "All leaders are: $LEADERS[@]"
