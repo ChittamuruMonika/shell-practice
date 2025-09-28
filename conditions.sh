8935f78ba90cc66d918bbeb0ca93ae86f3a2c7c9
ChittamuruMonika <chittamurumonika2001@gmail.com>
2025-09-28 21:09:25 +0530

    number equal or not

--- conditions.sh
+++ conditions.sh
@@ -3,9 +3,9 @@
 Number=$1
 
 if [ $Number -lt 10 ]; then
-    echo "Given $Number is less than 10"
+    echo "Given number $Number is less than 10"
 elif [ $Number -eq 10 ]; then
-    echo "Given $Number is equal to 10"
+    echo "Given number $Number is equal to 10"
 else
-    echo "Given $Number is greater than 10"
+    echo "Given number $Number is greater than 10"
 fi
