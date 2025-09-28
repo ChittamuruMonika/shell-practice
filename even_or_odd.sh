79cf338e8c81e17fbd1c043706b897239d51c2e4
ChittamuruMonika <chittamurumonika2001@gmail.com>
2025-09-28 21:19:06 +0530

    number equal or not

--- even_or_odd.sh
+++ even_or_odd.sh
@@ -2,7 +2,7 @@
 
 echo "Enter a number"
 read Number
-if [ ($Number%2) -eq 0 ]; then
+if [ (($Number%2)) -eq 0 ]; then
     echo "Given number $Number is even"
 else
     echo "Given number $Number is odd"
